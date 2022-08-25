8dd3cd62f24d5e46d3bc54a500ab1347c91fdb24
AlbuquerqueYohan <yohanannonce@gmail.com>
2022-08-25 20:00:48 +0200

    New admin page for edit computer (In progress)

--- src/Controller/Admin/AdminComputerController.php
+++ src/Controller/Admin/AdminComputerController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Ordinateurs;
+use App\Form\OrdinateurType;
 use App\Repository\OrdinateursRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Config\TwigConfig;
 
 class AdminComputerController extends AbstractController
 {
@@ -20,8 +23,17 @@ class AdminComputerController extends AbstractController
     #[Route('/admin', name: 'admin_computer_index')]
     public function index()
     {
-        $allComputer = $this->repository->findAll();
-        return $this->render('admin/computer/index.html.twig');
+        $computers = $this->repository->findAll();
+        return $this->render('admin/computer/index.html.twig', compact('computers'));
     }
 
+    #[Route('/admin/edit{id}', name: 'admin_computer_edit')]
+    public function edit(Ordinateurs $computers)
+    {
+        $form = $this->createForm(OrdinateurType::class, $computers);
+        return $this->render('admin/computer/edit.html.twig',[
+            'computers' => $computers,
+            'form' => $form->createView()
+        ]);
+    }
 }
