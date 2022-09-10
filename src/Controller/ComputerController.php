e39a502b5ac22a90de53a4ca66f7cac9d88f4bf4
AlbuquerqueYohan <yohanannonce@gmail.com>
2022-09-10 09:49:37 +0200

    Add commentary

--- src/Controller/ComputerController.php
+++ src/Controller/ComputerController.php
@@ -38,17 +38,18 @@ class ComputerController extends AbstractController
         // Si $request contient une key 'filter'
         if ($request->query->get('filter'))
         {
-            // Utilisation de la méthode findAndWhere avec les différents filtre passé par la vue
+            // Utilisation de la méthode findAndWhere avec les différents filtres passés par la vue
             $repository = $this->repository->findAndWhere($request->query->get('filter'));
         } else {
             // Sinon j'utilise la méthode findAll afin de retourner la liste de tous les ordinateurs
             $repository = $this->repository->findAll();
         }
         // Mise en place de la mise en page à l'aide de paginator
-        // Ici par défaut la page chargé sera la 1 et je descide d'afficher 8 ordinateurs
+        // Ici par défaut la page chargé sera la 1 et je decide d'afficher 8 ordinateurs
         $computers = $paginator->paginate($repository,
             $request->query->get('page', 1),
             8);
+        // création de la vue avec les différentes entités que je souhaite envoyer pour générer une vue dynamique
         return $this->render('computer/index.html.twig', [
             'marques' => $marquesRepository->findAll(),
             'type_stockages' => Ordinateurs::TYPE_STOCKAGE,
@@ -62,24 +63,26 @@ class ComputerController extends AbstractController
     #[Route('/computer/{slug}-{id}', name: 'computer_show', requirements: ['slug' => '[a-z0-9\-]*'])]
     public function show($slug, $id)
     {
+        // Je cherche ici un ordinateur seulement en fonction de son ID
         $computer = $this->repository->find($id);
         return $this->render('computer/show.html.twig', [
-            'ordinateurs' => $computer
+            'computer' => $computer
         ]);
     }
 
     #[Route('/compare', name: 'computer_compare')]
     public function compareComputer(Request $request)
     {
+        // Je récupere tous les ordinateurs
         $computer = $this->repository->findAll();
         $query = $request->query;
-
+        // Je cherche si le premier ordinateur possède un ID ou sinon je retourne null
         $ordinateur1 = $query->get('id1') ?
             $this->repository->find($query->get('id1')) : null;
-
+        // Je cherche si le second ordinateur possède un ID ou sinon je retourne null
         $ordinateur2 = $query->get('id2') ?
             $this->repository->find($query->get('id2')) : null;
-
+        // Je retourne les différentes entités à ma vue
         return $this->render('computer/compare.html.twig', [
             'ordinateurs' => $computer,
             'ordinateur1' => $ordinateur1,
